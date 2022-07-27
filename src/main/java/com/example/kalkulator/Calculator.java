333f8d72e1fbd0f83f83370bbdc860ab2fd474bd
Damian Pietryczko <damian.pietryczko@gmail.com>
2022-07-27 20:36:38 +0200

    Secound Commit

--- src/main/java/com/example/kalkulator/Calculator.java
+++ src/main/java/com/example/kalkulator/Calculator.java
@@ -2,9 +2,14 @@ package com.example.kalkulator;
 
 public class Calculator {
     public void addition(){
-
+        System.out.println("It works");
     }
     public void subtraction(){
 
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addition();
     }
 }
